a735af7c31a2ecb7f47f6b309bb7478fd76f6f5f
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-08-12 00:07:03 +0530

    Add car image in hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import Button from "./Button";
 
 const Hero = () => {
@@ -19,6 +20,17 @@ const Hero = () => {
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image
+            src="/hero.png"
+            alt="hero image"
+            fill
+            className="object-contain"
+          />
+          <div className="hero__image-overlay" />
+        </div>
       </div>
     </div>
   );

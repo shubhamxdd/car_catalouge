016f44623a22ff7f3a511cdb0da04b42ca182490
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-08-14 14:18:12 +0530

    Show button

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,6 +3,7 @@ import { CarProps } from "@/types";
 import { calculateCarRent } from "@/utils";
 import Image from "next/image";
 import { useState } from "react";
+import { Button } from ".";
 
 interface CarCardProps {
   car: CarProps;
@@ -11,6 +12,7 @@ interface CarCardProps {
 const CarCard = ({ car }: CarCardProps) => {
   const { city_mpg, year, make, model, drive, transmission } = car;
   const carRent = calculateCarRent(city_mpg, year);
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="car-card group">
       <div className="car-card__content">
@@ -50,16 +52,18 @@ const CarCard = ({ car }: CarCardProps) => {
             <p className="text-[14px]">{drive.toUpperCase()}</p>
           </div>
           <div className="flex flex-col justify-center items-center gap-2">
-            <Image
-              src="/gas.svg"
-              width={20}
-              height={20}
-              alt="gas"
-            />
-            <p className="text-[14px]">
-              {city_mpg} MPG
-            </p>
+            <Image src="/gas.svg" width={20} height={20} alt="gas" />
+            <p className="text-[14px]">{city_mpg} MPG</p>
           </div>
+        </div>
+        <div className="car-card__btn-container">
+          <Button
+            title="View More"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(true)}
+          />
         </div>
       </div>
     </div>

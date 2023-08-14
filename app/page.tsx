7a24757360b7720475a9adeab2afd76b8abd4d33
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-08-14 13:50:36 +0530

    Display CarCard compoenent if car exists

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
-import { Hero, SearchBar } from "@/components";
+import { CarCard, Filters, Hero, SearchBar } from "@/components";
+import { fetchCars } from "@/utils";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  const isDataEmpty = !allCars || allCars.length < 1 || !Array.isArray(allCars);
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -11,8 +16,26 @@ export default function Home() {
         </div>
         <div className="home__filters">
           <SearchBar />
-          <div className="home__filter-container">{/* filter */}</div>
+          <div className="home__filter-container">
+            {/* <Filters title="fuel" />
+            <Filters title="year" /> */}
+          </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">No results!</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );

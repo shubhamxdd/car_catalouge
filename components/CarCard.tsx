555eccccfb6422b914d627fbcde947e4bbb176cd
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-08-14 13:52:13 +0530

    Build CarCard component

---
+++ components/CarCard.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { CarProps } from "@/types";
+import { calculateCarRent } from "@/utils";
+import { useState } from "react";
+
+interface CarCardProps {
+  car: CarProps;
+}
+
+const CarCard = ({ car }: CarCardProps) => {
+  const { city_mpg, year, make, model, drive, transmission } = car;
+  const carRent = calculateCarRent(city_mpg, year);
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+      <p className="flex mt-6 text-[32px] font-extrabold">
+        <span className="self-start text-[14px] font-semibold">$</span>
+        {carRent}
+        <span className="self-start text-[14px] font-semibold">/day</span>
+      </p>
+    </div>
+  );
+};
+
+export default CarCard;
--- components/index.ts
+++ components/index.ts
@@ -1,8 +1,19 @@
 import Button from "./Button";
+import CarCard from "./CarCard";
+import Filters from "./Filters";
 import Footer from "./Footer";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import SearchBar from "./SearchBar";
 import SearchManufacturer from "./SearchManufacturer";
 
-export { Hero, Button, Navbar, Footer, SearchBar, SearchManufacturer };
+export {
+  Hero,
+  Button,
+  Navbar,
+  Footer,
+  SearchBar,
+  SearchManufacturer,
+  Filters,
+  CarCard,
+};

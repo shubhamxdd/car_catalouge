f91b134aedb9c56c71798dfcf28724dbe2a7cf95
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-08-14 13:49:16 +0530

    Create function to fetch cars and calculate car rent

---
+++ utils/index.ts
@@ -0,0 +1,30 @@
+export async function fetchCars() {
+  const headers = {
+    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_X_RapidAPI_Key,
+    "X-RapidAPI-Host": process.env.NEXT_PUBLIC_X_RapidAPI_Host,
+  };
+
+  const res = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    {
+      headers: headers,
+    }
+  );
+
+  const result = await res.json();
+
+  return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50;
+  const mileageFactor = 0.1;
+  const ageFactor = 0.05;
+
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};

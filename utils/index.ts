571a0ee181da530e08182bdb5bdb2b4a66d5d5bf
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-08-16 11:49:55 +0530

    Create function to fetch car images

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
   const headers = {
     "X-RapidAPI-Key": process.env.NEXT_PUBLIC_X_RapidAPI_Key,
@@ -24,4 +26,17 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImage = (car: CarProps, angle?: string) => {
+  const url = new URL("http://cdn.imagin.studio/getimage");
+  const { make, model, year } = car;
+  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
+
+  return `${url}`;
 };
